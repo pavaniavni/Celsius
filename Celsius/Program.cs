7b7f5800ae44832dd72d651bede31dd6849afc2d
pavani <pavaniavni456@gmail.com>
2022-02-08 12:57:51 +0530

    added celsius

---
+++ Celsius/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class Celsius
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the value of Celsius : ");
+        int cel = Convert.ToInt32(Console.ReadLine());
+        int Kel = cel + 273;
+        int Fah = cel * 18 / 10 + 32;
+        Console.WriteLine("The value of Kelvin = {0}", Kel);
+        Console.WriteLine("The value of Fahrenheit = {0}", Fah);
+    }
+}
